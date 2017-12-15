e69d15e80d77a70214c608295be282b0568c5f05
Kamiski.Lukasz <198151@edu.p.lodz.pl>
2017-12-15 20:15:02 +0100

    [testy_v1] Test : wartość jest ostatnim elementem w tablicy

--- src/test/java/bsearch/test/SearchResultTest.java
+++ src/test/java/bsearch/test/SearchResultTest.java
@@ -7,6 +7,8 @@ import edu.iis.mto.bsearch.SearchResult;
 
 public class SearchResultTest {
 
+    private final int KEY_NOT_FOUND = -1;
+
     @org.junit.Test
     public void keyIsInSequenceWithASizeEqualToOne() {
         final int key = 2;
@@ -21,10 +23,9 @@ public class SearchResultTest {
     public void keyIsNotInSequenceWithASizeEqualToOne() {
         final int key = 1;
         final int[] sequence = {2};
-        final int keyPositionInSequence = -1; // KEY not found
 
         SearchResult result = BinarySearch.search(key, sequence);
-        assertTrue(result.isFound() == false && result.getPosition() == keyPositionInSequence);
+        assertTrue(result.isFound() == false && result.getPosition() == KEY_NOT_FOUND);
     }
 
     @org.junit.Test
@@ -32,6 +33,16 @@ public class SearchResultTest {
         final int key = 1;
         final int[] sequence = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         final int keyPositionInSequence = 1;
+
+        SearchResult result = BinarySearch.search(key, sequence);
+        assertTrue(result.isFound() == true && result.getPosition() == keyPositionInSequence);
+    }
+
+    @org.junit.Test
+    public void keyIsLastElementInSequence() {
+        final int key = 10;
+        final int[] sequence = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        final int keyPositionInSequence = 10;
 
         SearchResult result = BinarySearch.search(key, sequence);
         assertTrue(result.isFound() == true && result.getPosition() == keyPositionInSequence);
