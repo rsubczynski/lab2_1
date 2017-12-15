fe3bddb5572f9d17115fe3c1893aed4c17d3679c
Kamiski.Lukasz <198151@edu.p.lodz.pl>
2017-12-15 20:11:45 +0100

    [testy_v1] Test : wartosć jest pierwszym elementem w sekwencji

--- src/test/java/bsearch/test/SearchResultTest.java
+++ src/test/java/bsearch/test/SearchResultTest.java
@@ -8,7 +8,7 @@ import edu.iis.mto.bsearch.SearchResult;
 public class SearchResultTest {
 
     @org.junit.Test
-    public void isInSequenceWithASizeEqualToOne() {
+    public void keyIsInSequenceWithASizeEqualToOne() {
         final int key = 2;
         final int[] sequence = {2};
         final int keyPositionInSequence = 1;
@@ -18,13 +18,23 @@ public class SearchResultTest {
     }
 
     @org.junit.Test
-    public void isNotInSequenceWithASizeEqualToOne() {
+    public void keyIsNotInSequenceWithASizeEqualToOne() {
         final int key = 1;
         final int[] sequence = {2};
         final int keyPositionInSequence = -1; // KEY not found
 
         SearchResult result = BinarySearch.search(key, sequence);
         assertTrue(result.isFound() == false && result.getPosition() == keyPositionInSequence);
+    }
+
+    @org.junit.Test
+    public void keyIsFirstElementInSequence() {
+        final int key = 1;
+        final int[] sequence = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        final int keyPositionInSequence = 1;
+
+        SearchResult result = BinarySearch.search(key, sequence);
+        assertTrue(result.isFound() == true && result.getPosition() == keyPositionInSequence);
     }
 
 }
