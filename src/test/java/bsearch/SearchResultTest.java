8caaa5d0805a1f1062480102021bd2331e673c2c
Chojnacki.Piotr <198144@edu.p.lodz.pl>
2017-12-16 17:17:18 +0100

    testy_v1: Nie ma w sekwencji o rozmiarze 1

--- src/test/java/bsearch/SearchResultTest.java
+++ src/test/java/bsearch/SearchResultTest.java
@@ -4,6 +4,7 @@ import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SearchResultTest {
@@ -16,6 +17,17 @@ public class SearchResultTest {
 
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertTrue(searchResult.isFound() && searchResult.getPosition() == positionInSequence);
+    }
+
+    @Test
+    public void isNotInSequenceOfSizeOne() {
+        final int key = 2;
+        final int[] seq = {3};
+        final int positionInSequence = -1;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertFalse(searchResult.isFound());
+        assertTrue(searchResult.getPosition() == positionInSequence);
     }
 
 }
