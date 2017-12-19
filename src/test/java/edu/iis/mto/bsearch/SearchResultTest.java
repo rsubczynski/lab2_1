2739ae689af81f4a84bedbf3dc6028c99416dd0d
jankowski.lukasz <191867@edu.p.lodz.pl>
2017-12-19 21:32:22 +0100

    testy_v1 - testy dla przypadków [jest pierwszym elementem w sekwencji], [jest ostatnim elementem w sekwencji] dla sekwencji dłuższej niż jednoelementowa

--- src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
+++ src/test/java/edu/iis/mto/bsearch/SearchResultTest.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.bsearch;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
@@ -45,6 +46,28 @@ public class SearchResultTest {
         SearchResult searchResult = BinarySearch.search(element, sequence);
         assertTrue(searchResult.getPosition() == positionWhenElementNotExist);
     }
+
+    @Test
+    public void elementIsFirsElementInMoreThanOneElementSequence(){
+        int element = 121;
+        int[] sequence = {121, 122, 123, 124, 125, 127};
+        int firstPositionInSequence = 1;
+
+        SearchResult searchResult = BinarySearch.search(element, sequence);
+        assertTrue(searchResult.getPosition() == firstPositionInSequence);
+    }
+
+    @Test
+    public void elementIsLastElementInMoreThanOneElementSequence(){
+        int element = 121;
+        int[] sequence = {117, 118, 119, 120, 121};
+        int lastPositionInSequence = sequence.length;
+
+        SearchResult searchResult = BinarySearch.search(element, sequence);
+        assertTrue(searchResult.getPosition() == lastPositionInSequence);
+    }
+
+
 
 
 
