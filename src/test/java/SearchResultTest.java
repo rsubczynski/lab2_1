3bd62a44a8f95c8ced810858438b8de4a14f20a1
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-13 17:08:01 +0100

    testy_v1: klucz nie powinien naleze do sekwencji pierwszej + poprawienie nazwy pierwszego testu

--- src/test/java/SearchResultTest.java
+++ src/test/java/SearchResultTest.java
@@ -3,18 +3,30 @@ import edu.iis.mto.bsearch.BinarySearch;
 import edu.iis.mto.bsearch.SearchResult;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SearchResultTest {
 
     @Test
-    public void isExistSequenceOfSizeOne() {
+    public void keyShouldExistSequenceOfSizeOne() {
         final int key = 3;
         final int[] seq = {3};
         final int positionInSequence = 1;
 
         SearchResult searchResult = BinarySearch.search(key, seq);
         assertTrue(searchResult.isFound() && searchResult.getPosition() == positionInSequence);
+    }
+
+    @Test
+    public void keyShouldntExistInSequenceOfSizeOne() {
+        final int key = 2;
+        final int[] seq = {3};
+        final int positionInSequence = -1;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertFalse(searchResult.isFound());
+        assertTrue(searchResult.getPosition() == positionInSequence);
     }
 
 }
