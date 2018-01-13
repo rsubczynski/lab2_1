4eb676e7d3171c3cfe40529f9b440bf02c6f0587
Radoslaw Subczynski <radoslaw.subczynski@abastra.com>
2018-01-13 17:00:55 +0100

    testy_v1 istnieje w sekcji o rozmiarze 1

---
+++ src/test/java/SearchResultTest.java
@@ -0,0 +1,20 @@
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SearchResultTest {
+
+    @Test
+    public void isExistSequenceOfSizeOne() {
+        final int key = 3;
+        final int[] seq = {3};
+        final int positionInSequence = 1;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        assertTrue(searchResult.isFound() && searchResult.getPosition() == positionInSequence);
+    }
+
+}
